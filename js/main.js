42713d02497690485c6fcbd4814cc078ea9c0aa1
laoLi <1060570952@qq.com>
2017-04-05 18:42:04 +0800

    博学谷分支抽取部分

--- js/main.js
+++ js/main.js
@@ -1,33 +1,109 @@
-/**
- * Created by Administrator on 2017-04-03.
- */
 define([], function () {
+	
+	// 这里配置模块的路径与依赖
+  require.config({
+    baseUrl: '/',
+    paths: {
 
-    //����
-    require.config({
-        baseUrl:'./',
-        paths:{
-            advertAdd:'js/advert/advert_add',
-            advertList:'js/advert/advert_list',
-            courseAdd1:'js/course/course_add_step1',
-            courseAdd2:'js/course/course_add_step2',
-            courseAdd3:'js/course/course_add_step3',
-            courseAdd:'js/course/course_add',
-            courseCategoreAdd:'js/course/course_category_add',
-            courseCategory:'js/course/course_category',
-            courseList:'js/course/course_list',
-            courseTopic:'js/course/course_topic',
-            login:'js/home/login',
-            repass:'js/home/repass',
-            settings:'js/home/settings',
-            teacherAdd:'js/teacher/teacher_add',
+      // 配置自己写的js模块别名
+      advertAdd: 'js/advert/advert_add',
+      advertList: 'js/advert/advert_list',
+      courseAdd1: 'js/course/course_add_step1',
+      courseAdd2: 'js/course/course_add_step2',
+      courseAdd3: 'js/course/course_add_step3',
+      courseAdd: 'js/course/course_add',
+      courseCategoryAdd: 'js/course/course_category_add',
+      courseCategory: 'js/course/course_category',
+      courseList: 'js/course/course_list',
+      courseTopic: 'js/course/course_topic',
+      login: 'js/home/login',
+      repass: 'js/home/repass',
+      settings: 'js/home/settings',
+      index: 'js/home/index',
+      teacherAdd: 'js/teacher/teacher_add',
+      teacherList: 'js/teacher/teacher_list',
+      userProfile: 'js/user/user_profile',
+      userList: 'js/user/user_list',
 
-            //���õ�����jsģ��ͱ���
-            template:'lib/artTemplate/template-debug',
+      // 配置第三方js模块别名
+      template: 'lib/artTemplate/template-debug',
+      bootstrap: 'lib/bootstrap/js/bootstrap',
+      datepicker: 'lib/bootstrap-datepicker/js/bootstrap-datepicker',
+      ckeditor: 'lib/ckeditor/ckeditor',
+      ckeditorLand: 'lib/ckeditor/lang/zh-cn',
+      echarts: 'lib/echarts/echarts.min',
+      jquery: 'lib/jquery/jquery.min',
+      jqueryCookie: 'lib/jquery-cookie/jquery.cookie',
+      jqueryForm: 'lib/jquery-form/jquery.form',
+      jqueryRegion: 'lib/jquery-region/jquery.region',
+      nprogress: 'lib/nprogress/nprogress',
+    },
 
-        }
-
-    })
-
-    //����ҳ����ض�Ӧ��JSģ��
-})
+    shim: {
+      //bootstrap是非define依赖于jquery
+      bootstrap: {
+        deps: ['jquery']
+      }
+    }
+  });
+  
+  // 这里根据页面的路径进行不同页面的区分进而加载不同的js
+  var pathname = location.pathname;
+  
+  switch(pathname) {
+  	case '/':
+  		require(['index']);
+  		break;
+	  case '/html/home/login.html':
+	  	require(['login']);
+  		break;
+	  case '/html/home/repass.html':
+	  	require(['repass']);
+  		break;
+		case '/html/home/settings.html':
+	  	require(['settings']);
+  		break;
+  	case '/html/advert/advert_add.html':
+  	  require(['advertAdd']);
+  		break;
+  	case '/html/advert/advert_list.html':
+  	  require(['advertList']);
+  		break;
+	  case '/html/course/course_add_step1.html':
+  	  require(['courseAdd1']);
+  		break;
+  	case '/html/course/course_add_step2.html':
+  	  require(['courseAdd2']);
+  		break;
+  	case '/html/course/course_add_step3.html':
+  	  require(['courseAdd3']);
+  		break;
+  	case '/html/course/course_add.html':
+  	  require(['courseAdd']);
+  		break;
+  	case '/html/course/course_category_add.html':
+  	  require(['courseCategoryAdd']);
+  		break;
+  	case '/html/course/course_category.html':
+  	  require(['courseCategory']);
+  		break;
+  	case '/html/course/course_list.html':
+  	  require(['courseList']);
+  		break;
+  	case '/html/course/course_topic.html':
+  	  require(['courseTopic']);
+  		break;
+  	case '/html/teacher/teacher_add.html':
+  	  require(['teacherAdd']);
+  		break;
+  	case '/html/teacher/teacher_list.html':
+  	  require(['teacherList']);
+  		break;
+  	case '/html/user/user_profile.html':
+  	  require(['userProfile']);
+  		break;
+  	case '/html/user/user_list.html':
+  	  require(['userList']);
+  		break;
+  }
+});
